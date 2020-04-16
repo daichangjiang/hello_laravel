30036b1c4faf0abe41548fb2324352283a49f021
daicj5 <daicj5@chinaunicom.cn>
2020-04-16 11:26:47 +0800

    基础页面

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>about</title>
-</head>
-
-<body>
-    <h1>关于</h1>
-</body>
-
-</html>
+@extends('layouts.default')
+@section('title', '关于')
+@section('content')
+<h1>关于</h1>
+@stop
